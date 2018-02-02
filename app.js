7e591adb889d2c287d88f645d37696f9a0d8804f
xiaofei.wang <979436452@qq.com>
2018-02-02 15:20:31 +0800

    add

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ export default class App extends Component {
                             defaultSelectedKeys={['2']}
                             style={{ lineHeight: '64px' }}
                         >
-                            <Menu.Item key="1">nav 1</Menu.Item>
+                            <Menu.Item key="1"><Link to="/">home</Link></Menu.Item>
                             <Menu.Item key="2">nav 2</Menu.Item>
                             <Menu.Item key="3">nav 3</Menu.Item>
                         </Menu>
